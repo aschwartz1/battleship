a0be005badbbdf5bb4f1174e6fc7257511d87760
Ben Fulton <benjamin.fulton99@gmail.com>
2020-12-07 21:04:59 -0700

    Add render for empty and miss conditionals

--- lib/cell.rb
+++ lib/cell.rb
@@ -23,4 +23,27 @@ class Cell
   def fire_upon
     @fired_upon = true
   end
+
+  def render
+    if render_empty?
+      '.'
+    elsif render_miss?
+      'M'
+    else
+      "Something went wrong!"
+    end
+  end
+  
+  private
+
+  def render_empty?
+    empty? && !fired_upon?
+  end
+
+  def render_miss?
+    empty? && fired_upon?
+  end
+
+
+
 end
