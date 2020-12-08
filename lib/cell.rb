09d7a79a418f7b9f49002a073679a46dfe9fd2aa
Ben Fulton <benjamin.fulton99@gmail.com>
2020-12-07 20:32:17 -0700

    Add empty and placement methods:

--- lib/cell.rb
+++ lib/cell.rb
@@ -4,6 +4,15 @@ class Cell
 
   def initialize(coordinate)
     @coordinate = coordinate
+    @ship = nil
+  end
+
+  def empty?
+    @ship.nil?
+  end
+
+  def place_ship(ship_obj)
+    @ship = ship_obj
   end
 
 end
