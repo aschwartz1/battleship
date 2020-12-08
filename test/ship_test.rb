f64f9f43b066767096c46297cb47aa62f0f993b7
Alex Schwartz <aschwartz@detaso.com>
2020-12-07 20:45:26 -0600

    Add attribute test

--- test/ship_test.rb
+++ test/ship_test.rb
@@ -10,4 +10,10 @@ class ShipTest < MiniTest::Test
   def test_it_exists
     assert_instance_of Ship, @cruiser
   end
+
+  def test_readable_attributes
+    assert_equal "Cruiser", @cruiser.name
+    assert_equal 3, @cruiser.length
+    assert_equal 3, @cruiser.health
+  end
 end
