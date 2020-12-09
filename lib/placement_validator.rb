7383982e54dbdbc07db7a9dc764146a997c03e81
Ben Fulton <benjamin.fulton99@gmail.com>
2020-12-08 19:52:04 -0700

    Add validation helper method structure

---
+++ lib/placement_validator.rb
@@ -0,0 +1,20 @@
+require './lib/ship'
+
+class PlacementValidator
+  def is_valid_length?(ship, coords)
+
+  end
+
+  def is_consecutive?(ship, coords)
+
+  end
+
+  def not_diagonal?(ship, coords)
+    #make sure the return value wants to be true
+  end
+
+  def valid_placement?(ship, coords)
+
+  end
+
+end
