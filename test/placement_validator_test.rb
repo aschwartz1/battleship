eeea0c2b5d7a19822b9641dc02ae06bd3ac7f994
Ben Fulton <benjamin.fulton99@gmail.com>
2020-12-08 20:08:52 -0700

    Add validation state tests

--- test/placement_validator_test.rb
+++ test/placement_validator_test.rb
@@ -7,10 +7,19 @@ require './lib/placement_validator'
 class PlacementValidatorTest < MiniTest::Test
   def setup
     @validator = PlacementValidator.new
+    @cruiser = Ship.new("Cruiser", 3)
+    @submarine = Ship.new("Submarine", 2)
   end
 
   def test_it_is
     assert_instance_of PlacementValidator, @validator
   end
 
+  def test_is_valid_length
+    assert_equal true, @validator.is_valid_length?(@submarine,["A1","A2"])
+  end
+
+  def test_is_NOT_valid_length
+    assert_equal false, @validator.is_valid_length?(@submarine,["A1","A2","A3"])
+  end
 end
