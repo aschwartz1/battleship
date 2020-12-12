0cc422152d8830bfd68d3d4e7aaaf103271d52ea
Ben Fulton <benjamin.fulton99@gmail.com>
2020-12-11 21:54:42 -0700

    Add game logic

--- lib/game.rb
+++ lib/game.rb
@@ -4,8 +4,12 @@ require './lib/turn'
 
 class Game
   def initialize(player_board,cpu_board,turn)
-    @cruiser = Ship.new("Cruiser", 3)
-    @submarine = Ship.new("Submarine", 2)
+    @cpu_cruiser = Ship.new("Cruiser", 3)
+    @cpu_submarine = Ship.new("Submarine", 2)
+
+    @player_cruiser = Ship.new("Cruiser", 3)
+    @player_submarine = Ship.new("Submarine", 2)
+
     @player_board = player_board
     @cpu_board = cpu_board
     @turn = turn
@@ -13,12 +17,32 @@ class Game
 
   def start
     puts "Welcome to Battleship!\n"
-    puts @player_board.render
-    player_cruiser_coords = get_player_placement(@cruiser)
-    player_sub_coords = get_player_placement(@submarine)
-    binding.pry
-    cpu_cruiser_coords = valid_cpu_cruiser_placement
+    puts @player_board.render(true)
 
+    get_player_placement(@player_cruiser)
+    get_player_placement(@player_submarine)
+    valid_cpu_cruiser_placement
+    valid_cpu_sub_placement
+    @turn.play until game_over?
+
+    if cpu_win?
+      puts "You lost!"
+    elsif player_win?
+      puts "You won!"
+    end
+
+  end
+
+  def game_over?
+    cpu_win? || player_win?
+  end
+
+  def cpu_win?
+    @player_board.has_lost?
+  end
+
+  def player_win?
+    @cpu_board.has_lost?
   end
 
   def player_in
@@ -45,12 +69,17 @@ class Game
 
   def valid_cpu_cruiser_placement
     coordinate_check = ["#{cpu_random_coordinate}" ,"#{cpu_random_coordinate}","#{cpu_random_coordinate}" ]
-    until @cpu_board.placement_validator.validate(@cruiser, coordinate_check)
+    until @cpu_board.cpu_place(@cpu_cruiser, coordinate_check)
       coordinate_check = ["#{cpu_random_coordinate}" ,"#{cpu_random_coordinate}","#{cpu_random_coordinate}" ]
     end
-    coordinate_check
-    #some code here for either shooting in the dark
-    # or generating only valid coordinates
+  end
+
+  def valid_cpu_sub_placement
+    coordinate_check = ["#{cpu_random_coordinate}" ,"#{cpu_random_coordinate}"]
+    until @cpu_board.cpu_place(@cpu_submarine, coordinate_check)
+      coordinate_check = ["#{cpu_random_coordinate}" ,"#{cpu_random_coordinate}"]
+    end
+    #REWORK
   end
 
 end
