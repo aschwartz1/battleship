741cc1207fa187932762f1e4c44ca20e5e6add22
Ben Fulton <benjamin.fulton99@gmail.com>
2020-12-11 21:56:32 -0700

    Add stages of gameplay methods

--- lib/turn.rb
+++ lib/turn.rb
@@ -2,11 +2,49 @@ require './lib/board'
 require './lib/ship'
 
 class Turn
-  def initialize(player, cpu)
+  def initialize(player_board, cpu_board)
     @cruiser = Ship.new("Cruiser", 3)
     @submarine = Ship.new("Submarine", 2)
-    @player = player
-    @cpu = cpu
+    @player_board = player_board
+    @cpu_board = cpu_board
+  end
+
+  def play
+    puts game_state
+    player_shot
+    cpu_shot
+    puts "Your shot was whatever"
+    puts "my shot was whatever"
+  end
+
+  def player_shot
+    puts "Which cell would you like to fire on"
+    player_target = gets.chomp.to_s.upcase
+    until @cpu_board.coordinate_exists_on_board?(player_target) && !@player_board.blacklist.include?(player_target)
+      puts "Invalid target"
+      player_target = gets.chomp.to_s.upcase
+    end
+    @cpu_board.cells[player_target].fire_upon
+    @player_board.blacklist << player_target
+  end
+
+  def cpu_shot
+    cpu_target = cpu_random_coordinate
+    until @player_board.coordinate_exists_on_board?(cpu_target) && !@cpu_board.blacklist.include?(cpu_target)
+      cpu_target = cpu_random_coordinate
+    end
+    @player_board.cells[cpu_target].fire_upon
+    @cpu_board.blacklist << cpu_target
+  end
+
+  def cpu_random_coordinate
+    #MOVE FROM GAME TO HERE
+    (["A", "B", "C", "D"].shuffle)[rand(0..3)] + rand(1..4).to_s
+  end
+
+  def game_state
+    puts @player_board.render(true)
+    puts @cpu_board.render
   end
 
 end
